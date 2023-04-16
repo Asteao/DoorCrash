7d8ee81a257e13ae7f29db37de3fa91d462c7365
Martin <asteao@gmail.com>
2023-04-15 17:00:07 -0700

    Updating new order behaviour

---
+++ Assets/handleOrder.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class handleOrder : MonoBehaviour
+{
+    public GameObject smallOrder;
+    public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+       canvas = GameObject.Find("Canvas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.Y))
+        {
+            Instantiate(smallOrder, canvas.transform);
+            Destroy(this.gameObject);
+        }
+
+         if (Input.GetKeyDown(KeyCode.N))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
